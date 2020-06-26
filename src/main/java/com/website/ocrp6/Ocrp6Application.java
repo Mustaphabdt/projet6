b9c9ce39e2ace4b521b47f9324e49e6ad4f8f3e8
Mustaphabdt <boudount@gmail.com>
2020-06-26 16:58:44 +0200

    first commit

---
+++ src/main/java/com/website/ocrp6/Ocrp6Application.java
@@ -0,0 +1,13 @@
+package com.website.ocrp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ocrp6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ocrp6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/website/ocrp6/Ocrp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.ocrp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ocrp6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
